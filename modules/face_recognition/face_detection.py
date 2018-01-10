7afda3c54c7eaf051603009ba38009d4b43e3767
Grigor <grigortovmasian@gmail.com>
2018-01-10 21:57:06 +0400

    Face recognition updates
    Signed-off-by: Grigor <grigortovmasian@gmail.com>

--- modules/face_recognition/face_detection.py
+++ modules/face_recognition/face_detection.py
@@ -11,34 +11,95 @@ class face_detection(object):
         self.camera.resolution = (320, 240)
     def __delete__(self):
         pass
+    
+    def detect_lower_body(self):
+        self.get_pic()
+        self.get_opencv()
+
+        #Load a cascade file for detecting faces
+        self.face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_lowerbody.xml')
+
+        self.detect_object()
+        self.save_pic("lower_body.jpg")
+
+    def detect_eye(self):
+        self.get_pic()
+        self.get_opencv()
+
+        #Load a cascade file for detecting faces
+        self.face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_eye.xml')
+
+        self.detect_object()
+        self.save_pic("eye.jpg")
+
+    def detect_upper_body(self):
+        self.get_pic()
+        self.get_opencv()
+
+        #Load a cascade file for detecting faces
+        self.face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_upperbody.xml')
+
+        self.detect_object()
+        self.save_pic("upper_body.jpg")
+
+    def detect_full_body(self):
+        self.get_pic()
+        self.get_opencv()
+
+        #Load a cascade file for detecting faces
+        self.face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_fullbody.xml')
+
+        self.detect_object()
+        self.save_pic("full_body.jpg")
+
+    def detect_smile(self):
+        self.get_pic()
+        self.get_opencv()
+
+        #Load a cascade file for detecting faces
+        self.face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_smile.xml')
+
+        self.detect_object()
+        self.save_pic("smile.jpg")
+
     def detect_face(self):
+        self.get_pic()
+        self.get_opencv()
+
+        #Load a cascade file for detecting faces
+        self.face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
+
+        self.detect_object()
+        self.save_pic("face.jpg")
+
+
+    #using functions
+    def get_pic(self):
         #Get the picture (low resolution, so it should be quite fast)
-	#Here you can also specify other parameters (e.g.:rotate the image)
+        #Here you can also specify other parameters (e.g.:rotate the image)
         self.camera.capture(self.stream, format='jpeg')
-        
+    
+    def get_opencv(self):
         #Convert the picture into a numpy array
         buff = numpy.fromstring(self.stream.getvalue(), dtype=numpy.uint8)
 	
         #Now creates an OpenCV image
         self.image = cv2.imdecode(buff, 1)
-	
-        #Load a cascade file for detecting faces
-        face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
-	
+
+    def detect_object(self):
         #Convert to grayscale
         gray = cv2.cvtColor(self.image,cv2.COLOR_BGR2GRAY)
 	
         #Look for faces in the image using the loaded cascade file
-        faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+        self.faces = self.face_cascade.detectMultiScale(gray, 1.1, 5)
 	
-        print ("Found "+str(len(faces))+" face(s)")
+        print ("Found "+str(len(self.faces))+" object(s)")
 	
         #Draw a rectangle around every found face
-        for (x,y,w,h) in faces:
+        for (x,y,w,h) in self.faces:
             cv2.rectangle(self.image,(x,y),(x+w,y+h),(255,255,0),2)
-        
-        self.save_pic("result.jpg")
-    
+   
+
     def save_pic(self, fileName):
         #Save the result image
         cv2.imwrite(fileName,self.image)
